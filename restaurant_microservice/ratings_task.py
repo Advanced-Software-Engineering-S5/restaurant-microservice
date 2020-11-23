9e84ef4e87891c52da908b4512ceed0382a17f28
NickLucche <nicolo.lucchesi@gmail.com>
2020-11-24 00:32:00 +0100

    added and tested scheduler for periodic task

--- restaurant_microservice/ratings_task.py
+++ restaurant_microservice/ratings_task.py
@@ -1,7 +1,7 @@
 from os import environ
 from restaurant_microservice.background import celery
 from restaurant_microservice.database import Restaurant, Review, db
-import os
+import logging
 
 @celery.task
 def average_review_stars():
@@ -12,10 +12,17 @@ def average_review_stars():
     # get all un-counted reviews
     reviews = Review.query.filter_by(marked=False).join(Restaurant)\
         .with_entities(Review, Restaurant).all()
+    logging.info(f"Averaging review stars of {len(reviews)} retrieved reviews..")
     for review, restaurant in reviews:
         # compute running mean of reviews
         restaurant.num_reviews += 1
         restaurant.avg_stars = 1/restaurant.num_reviews * \
             (restaurant.avg_stars * (restaurant.num_reviews-1) + review.stars)
+        review.marked = True
     # update rows            
     db.session.commit() 
+
+@celery.on_after_finalize.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Register the average review star task
+    sender.add_periodic_task(60.0, average_review_stars.s(), name='average_review_stars')
